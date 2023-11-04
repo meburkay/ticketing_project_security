1a540d41a29419e6376ff13045aebf07626d3676
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-11-04 15:44:32 +0300

    we create our custom user mapper for converting our user to spring's user

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -19,23 +19,23 @@ import java.util.List;
 public class SecurityConfig {
 
     //We are creating bean for Spring user part here. Normally it give a username and password but by creating this bean we are overriding it and create our own user and passwords.
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
-
-        List<UserDetails> userList = new ArrayList<>();
-
-        //The adding new User is spring framework user not our user class. And as a constructor we must give 3 argument. One is username, 2 is encoded password(Because of that we encode id) andd granted authority(As a naming convention that spring understand we must put ROLE_ at the start of the argument.
-        userList.add(
-                new User("mike",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
-        );
-
-        userList.add(
-                new User("ozzy",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")))
-        );
-
-        return new InMemoryUserDetailsManager(userList);
-
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+//
+//        List<UserDetails> userList = new ArrayList<>();
+//
+//        //The adding new User is spring framework user not our user class. And as a constructor we must give 3 argument. One is username, 2 is encoded password(Because of that we encode id) andd granted authority(As a naming convention that spring understand we must put ROLE_ at the start of the argument.
+//        userList.add(
+//                new User("mike",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
+//        );
+//
+//        userList.add(
+//                new User("ozzy",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")))
+//        );
+//
+//        return new InMemoryUserDetailsManager(userList);
+//
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
---
+++ src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -0,0 +1,64 @@
+package com.cydeo.entity.common;
+
+import com.cydeo.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+//This class created for mapping our user to spring user. Because of that we implement the UserDetails interface that spring uses for its operations and then by overriding its methods we convert our user object to spring user object.
+public class UserPrincipal implements UserDetails {
+
+    //this user is our user not from spring.
+    private User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+
+        //this is like the one at the securityconfig part that we commented. But the difference is this is not hardcoded. this take the user from database and get its descriptions employee or whatever and used it when giving authority.
+        GrantedAuthority authority = new SimpleGrantedAuthority(this.user.getRole().getDescription());
+
+        authorityList.add(authority);
+
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassWord();   //how i can acccess to password field of the user object
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.isEnabled();
+    }
+}
