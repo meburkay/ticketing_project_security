b92b87760774d29555f9339382efbbf6907e6ec6
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-11-04 14:12:08 +0300

    create PasswordEncoder bean for using to convert the user's password.

--- src/main/java/com/cydeo/TicketingProjectSecurityApplication.java
+++ src/main/java/com/cydeo/TicketingProjectSecurityApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication  //this includes @Configuration
 public class TicketingProjectSecurityApplication {
@@ -17,5 +19,12 @@ public class TicketingProjectSecurityApplication {
     public ModelMapper mapper(){
         return new ModelMapper();
     }
+
+    //Spring use encoded passwords. Here we create a spring framework class bean that take the raw password and convert it to encoded type. Here PassswordEncoder is interface and it has bunch of implementations. BCryptPasswordEncoder is one of them which is mostly used. Because of that we create the bean from it.
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
