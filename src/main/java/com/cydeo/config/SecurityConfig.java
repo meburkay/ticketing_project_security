a2ba628d395fd1b9c3f1043b0d6f114fe4ca2058
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-11-04 15:10:20 +0300

    authorization and authentication arrangements created for login

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,6 +36,41 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(userList);
 
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeRequests()
+                .antMatchers("/user/**").hasRole("ADMIN")//means user that have the role as ADMIN can do all the operations under user controller. The same logic below.
+                .antMatchers("/project/**").hasRole("MANAGER")
+                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
+                .antMatchers("/task/**").hasRole("MANAGER")
+//                .antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN") //We can give more than one role by this way.
+//                .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE") /When we use authority we must use ROLE_ convention. For hasRole it automatically added it so we do not need to use it.
+                .antMatchers(//this used for excluding the endpoints controller or folders from authentication. It is used with permitAll().
+                        "/",
+                        "/login",
+                        //below are the folders that we give authorization. ** is everything inside that folder.
+                        "/fragments/**",
+                        "/assets/**",
+                        "/images/**"
+                ).permitAll()
+
+                .anyRequest().authenticated()//means other than above must be authenticated.
+                .and()
+
+                //.httpBasic()//this defines the default html file that we see to authenticate. In the future we use our login page instead of this.
+                //Instead of httpBasic we arrange our login page here.
+                .formLogin()
+                .loginPage("/login")//We define here which page will we use to login.
+                .defaultSuccessUrl("/welcome")//Here we define where to go after login successfully.
+                .failureUrl("/login?error=true")//This is for failure.
+                .permitAll()
+
+                .and().build();
+    }
+
 
 
 }
